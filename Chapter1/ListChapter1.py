f66d1dfd555d46325687fe5134ac039ba7a2e8e0
JimboPower <edoardo.decal@gmail.com>
2016-01-01 18:30:36 +0100

    Finished exercises about lists and the for-in -suites

---
+++ Chapter1/ListChapter1.py
@@ -0,0 +1,15 @@
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+            ["Graham Chapman", ["Michael Palin", "John Cleese",
+                "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+
+for item in movies:
+    if isinstance(item, list):
+        for sub_item in item:
+            if isinstance(sub_item, list):
+                for sub_sub_item in sub_item:
+                    print(sub_sub_item)
+            else:
+                print(sub_item)
+    else:
+        print(item)
