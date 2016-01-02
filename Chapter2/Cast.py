f848672d232494b14cf559756929ba920f6b4138
JimboPower <edoardo.decal@gmail.com>
2016-01-02 12:18:01 +0100

    Added Range.py

--- Chapter2/Cast.py
+++ Chapter2/Cast.py
@@ -1,3 +1,5 @@
+import nester
+
 familia = ["Edoardo", "Giulia", "Ettore", "Cesare", ["Milou"], ["DeCal"]]
 
 nester.print_lol(familia)
--- Chapter2/nesterEdo/setup.py
+++ Chapter2/nesterEdo/setup.py
@@ -4,7 +4,7 @@ setup (
     name         = 'Edoardo',
     version      = '1.0.0',
     py_modules   = ['nester'],
-    autho        = 'Edoardo',
+    author        = 'Edoardo',
     author_email = 'hfpython@headfirstlabs.com',
     url          = 'http://www.headfirstlabs.com',
     description  = 'A simple printer of nasted lists',
