0f5c8726d62ccfcf52e4e89e6d099fed2a7a8c6e
JimboPower <edoardo.decal@gmail.com>
2016-01-04 18:57:07 +0100

    New Commit

--- Chapter 4/dataSketch.py
+++ Chapter 4/dataSketch.py
@@ -1,16 +1,22 @@
+man = []
 
+other = []
 
 try:
     data = open('sketch.txt')
     for each_line in data:
         try:
             (role, line_spoken) = each_line.split(':', 1)
-            print(role, end='')
-            print(' said ', end='')
-            print(line_spoken, end='')
+            line_spoken = line_spoken.strip()
+            if role == 'Man':
+                man.append(line_spoken)
+            elif role == 'Other Man':
+                other.append(line_spoken)
         except ValueError:
             pass  
     data.close()
 except IOError:
 
     print('The data file is missing!')
+print(man)
+print(other)
