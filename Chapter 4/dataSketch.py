b02cb2e9ff2ebfee3916c1716a48f75a3562d3de
JimboPower <edoardo.decal@gmail.com>
2016-01-09 12:21:43 +0100

    Save chapter 4

--- Chapter 4/dataSketch.py
+++ Chapter 4/dataSketch.py
@@ -15,9 +15,7 @@ try:
             pass  
     data.close()
 except IOError:
-
     print('The data file is missing!')
-
 try:
      with open("man_data.txt", "w") as man_file, open("other_data.txt", "w") as other_file:
         print(man, file=man_file)
