049b2accaffa0721d266471307bab9838b65f8f3
JimboPower <edoardo.decal@gmail.com>
2016-01-26 16:27:43 +0100

    Added chapter5

---
+++ Chapter5/SortData.py
@@ -0,0 +1,9 @@
+a = input('Inserisci il primo numero: ')
+b = input('Inserisci il secondo numero: ')
+c = input('Inserisci il terzo numero: ')
+d = input('Inserisci il quarto numero: ')
+e = input('Inserisci il quinto numero: ')
+f = input('Inserisci il sesto numero: ')
+data = [a, b, c, d, e, f]
+data2 = sorted(data)
+print(data2)
