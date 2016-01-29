12e3ef8f1f4e5bd4327f624be52ed166a4a22fd7
JimboPower <edoardo.decal@gmail.com>
2016-01-29 20:59:47 +0100

    Added DefSanitaze

---
+++ Chapter5/defSanitize.py
@@ -0,0 +1,14 @@
+
+def sanitize(time_string):
+
+    if '-' in time_string:
+        splitter = '-'
+        (mins, secs) = time_string.split(splitter)
+    elif ':' in time_string:
+        splitter = ':'
+        (mins, secs) = time_string.split(splitter)
+    else:
+        return(time_string)
+
+    
+        return(mins + '.' + secs)
