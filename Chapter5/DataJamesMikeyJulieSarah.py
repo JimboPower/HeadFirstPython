cfc179dc20c479b9a1c5c7d6fc21c34c0740a050
JimboPower <edoardo.decal@gmail.com>
2016-02-14 20:45:04 +0100

    Added chapter 6

--- Chapter5/DataJamesMikeyJulieSarah.py
+++ Chapter5/DataJamesMikeyJulieSarah.py
@@ -10,62 +10,12 @@ def sanitize(time_string):
 
 def coach_data(file_name):
     try:       
-        with open(filename) as coach_file:
-            word = coach_file.readline()
-            return(word.split(','))
+        with open(filename) as f:
+            data = f.readline()
+            return(data.strip().split(','))
     except IOError as ioerr:
                print("Files Error!!" + str(ioerr))
                return(None)
 
-with open('james.txt') as jaf:
-    data = jaf.readline()
-james = data.strip().split(',')
-
-with open('julie.txt') as juf:
-    data = juf.readline()
-julie = data.strip().split(',')
-
-with open('mikey.txt') as mif:
-    data = mif.readline()
-mikey = data.strip().split(',')
-
-with open('sarah.txt') as saf:
-    data = saf.readline()
-sarah = data.strip().split(',')
-
-
-james = coach_data('james.txt')
-sarah = coach_data('sarah.txt')
-mikey = coach_data('mikey.txt')
-julie = coach_data('julie.txt')
-
-james = sorted([sanitize(each_t) for each_t in james] [0:3])
-julie = sorted([sanitize(each_t) for each_t in julie] [0:3])
-mikey = sorted([sanitize(each_t) for each_t in mikey] [0:3])
-sarah = sorted([sanitize(each_t) for each_t in sarah])
-
-unique_james = []              
-unique_julie = []
-unique_mikey = []
-unique_sarah = []
-
-for each_t in james:
-    if each_t not in unique_james:
-        unique_james.append(each_t)
-
-for each_t in julie:
-    if each_t not in unique_julie:
-        unique_julie.append(eachy_t)
+coach_data('sarah2.txt')
     
-for each_t in mikey:
-    if each_t not in unique_mikey:
-        unique_mikey.append(each_t)
-
-for each_t in sarah:
-    if each_t not in unique_sarah:
-        unique_sarah.append(each_t)
-
-print(unique_james[0:3])
-print(unique_julie[0:3])
-print(unique_mikey[0:3])
-print(unique_sarah[0:3])
